b033c25670cd5da3ec2963e4d47abc24b242a054
sidak003 <sidakdhingra100@gmail.com>
2023-09-07 01:33:02 +0530

    added-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Merriweather, Inter } from "next/font/google";
+import Script from "next/script";
 
 const merriweather = Merriweather({
   weight: ["700"],
@@ -20,6 +21,15 @@ export const metadata: Metadata = {
   title: "Coming Soon | The Startup Company",
   description:
     "We aim to build the largest and most ambitious startup community in India. Join the Startup Revolution Where Visionaries Unite",
+  keywords: [
+    "startup",
+    "startup community",
+    "the startup company",
+    "founder's community",
+  ],
+  verification: {
+    google: "u56UBUsH9bmU42AnL1cXuFLwOTcmf6n_b9QnpI8nwZ4",
+  },
 };
 
 export default function RootLayout({
@@ -33,6 +43,18 @@ export default function RootLayout({
       className={`${inter.variable} ${merriweather.variable} scroll-smooth`}
     >
       <body>{children}</body>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env["GA_MEASUREMENT_ID"]}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env["GA_MEASUREMENT_ID"]}');
+        `}
+      </Script>
     </html>
   );
 }
